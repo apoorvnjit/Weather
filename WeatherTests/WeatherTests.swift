edf01762ecf0b2b3f4a21982a16a9f7677fd6ea3
Apoorva Reed(Personal) <ar965@njit.edu>
2019-08-17 11:43:04 -0400

    Test case added

--- WeatherTests/WeatherTests.swift
+++ WeatherTests/WeatherTests.swift
@@ -19,11 +19,17 @@ class WeatherTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testWeatherViewModel() {
+        
+        let main = Main(temp: 23.3, tempMin: 19.5, tempMax: 24.0)
+        let sys = Sys(sunrise: 1566036574, sunset: 1566086012)
+        let weather: [Weather] = [Weather(id: 3, main: "Clear", description: "Clear Sky"), Weather(id: 3, main: "Clear", description: "Clear Sky")]
+        let currentWeatehr = CurrentWeatherModel(weather: weather, main: main, dt: 1566054761, sys: sys, name: "Newark", timezone: -14400)
+        
+        let currentViewModels = WeaterViewModel(currentWeather: currentWeatehr)
+        XCTAssertEqual(currentViewModels.dayNight, "Morning")
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
